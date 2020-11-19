03fdae297ef2530c528b2b5620e1c3df45db7b4f
Phil Prasek <prasek@gmail.com>
2020-11-19 14:54:52 -0800

    hack bump to v1.0.0-beta.2
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .projenrc.js
+++ .projenrc.js
@@ -3,13 +3,14 @@ const { ConstructLibraryCdk8s } = require('projen');
 const project = new ConstructLibraryCdk8s({
   authorAddress: "benisrae@amazon.com",
   authorName: "Elad Ben-Israel",
-  cdk8sVersion: "0.33.0",
-  name: "play-14140",
-  repository: "https://github.com/benisrae/play-14140.git",
+  cdk8sVersion: "1.0.0-beta.2",
+  name: "crossplane-cdk",
+  repository: "https://github.com/eladb/crossplane-cdk.git",
   defaultReleaseBranch: "main"
 });
 
-project.addPeerDeps('constructs@^3.0.4');
+project.addPeerDeps('constructs@^3.2.34');
+project.addPeerDeps('cdk8s-plus-17@^1.0.0-beta.2');
 
 project.eslint.addRules(
   {'max-len': ['error', {
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# my project
+# crossplane-cdk
+
+### notes
+- npx projen new
+- cdk8s import github:crossplane/crossplane@0.14.0
+- create tests
+- yarn test
+- create app
+- yarn compile
+- npx ts-node src/index.ts
+- check /dist
+
+Updating
+- pj
