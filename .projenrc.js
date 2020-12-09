8406bff135cdee9157c8e6c2ca369be238117804
Phil Prasek <prasek@gmail.com>
2020-12-09 08:36:11 -0800

    update meta
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,13 @@
 const { ConstructLibraryCdk8s } = require('projen');
 
 const project = new ConstructLibraryCdk8s({
-  authorAddress: "benisrae@amazon.com",
-  authorName: "Elad Ben-Israel",
+  authorAddress: "prasek@gmail.com",
+  authorName: "Phil Prasek",
   cdk8sVersion: "1.0.0-beta.3",
   name: "crossplane-cdk",
   description: 'CDK for Crossplane',
-  repository: "https://github.com/eladb/crossplane-cdk.git",
-  defaultReleaseBranch: "main",
+  repository: "https://github.com/crossplane-contrib/crossplane-cdk.git",
+  defaultReleaseBranch: "master",
   peerDeps: [
     'constructs@^3.2.42',
     'cdk8s-plus-17@^1.0.0-beta.3',
